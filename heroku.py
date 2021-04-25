c7c147c0ce622b4fe6c302c94f2aade0f2669658
apvkarthick <76872683+apvkarthick@users.noreply.github.com>
2021-04-25 16:18:24 +0530

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -6,6 +6,7 @@ import pandas as pd
 #gc = gspread.authorize(GC.get_application_default())
 from oauth2client.service_account import ServiceAccountCredentials
 from os import environ
+from flask import Flask, request, abort
 #'O:\\downloads-nov-2018\\python-self-programs\\akarthick-sheets-api.json'
 
 scope = ['https://spreadsheets.google.com/feeds',
@@ -16,3 +17,18 @@ master_sh=gc.open_by_key("1JTJlCdD1k96WUxkxuBq7OU7btBZqKxny9x8p9lo5IU0")
 master_worksheet = master_sh.worksheet("Sheet2")
 dfkeys = pd.DataFrame(master_worksheet.get_all_values()[1:11])
 print(dfkeys.columns)
+
+
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
+#57e0107c5767cc545a3e6d9480cbf411f57fe48a31cabe06f2bbb5250869
+if __name__ == '__main__':
+    app.run()
